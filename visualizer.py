0faf379f3c346f331a43a326a7ce5bd1d8e7df5b
Yusung Sim <yusungsim@kaist.ac.kr>
2021-12-01 20:58:57 +0900

    added visualizer

---
+++ visualizer.py
@@ -0,0 +1,38 @@
+import spacy
+from spacy import displacy
+from data_loader import get_reviews
+
+nlp = spacy.load("en_core_web_sm")
+
+def visualize_text(text):
+   doc = nlp(text)
+   displacy.serve(doc)
+
+def has_words(words, text):
+    for word in words:
+        if word in text:
+            return True
+    return False
+
+if __name__ == "__main__":
+    appname = 'com.sega.soccer.manager' 
+    reviews = get_reviews(appname)
+    target_words = [
+        'game',
+        'app',
+        'gameplay',
+        'money',
+        'price',
+        'bug',
+        'story',
+        'character',
+        'graphic',
+        'update',
+    ]
+
+    for review in reviews:
+        if not has_words(target_words, review):
+            continue
+        print(review)
+        visualize_text(review)
+        input()
